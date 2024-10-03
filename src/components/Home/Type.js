322e007647f01f9e353d979d0f0b57adb54d5499
Harshavardhan0727 <kasiharshav2@gmail.com>
2024-10-03 16:38:03 -0700

    Initial push

---
+++ src/components/Home/Type.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Fullstack Developer",
+          "Backend Developer",
+          "FrontEnd Developer",
+          "Open Source Contributor",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 70,
+      }}
+    />
+  );
+}
+
+export default Type;
